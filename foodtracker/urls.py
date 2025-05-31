71b7088aec28bebfe11ae38c4af23771b4b80778
Anthony Honstain <honstain@gmail.com>
2025-05-31 09:11:40 -0700

    Saving for posterity, windsurf came up with this to split up the list from graph, I don't have time to look through it but its pretty difficult to grok

--- foodtracker/urls.py
+++ foodtracker/urls.py
@@ -3,5 +3,7 @@ from foodtracker import views
 
 urlpatterns = [
     path("", views.list_food_logs, name="list_food_logs"),
+    path("chart/", views.food_logs_chart, name="food_logs_chart"),
+    path("chart/data/", views.food_logs_chart_data, name="food_logs_chart_data"),
     path("add/", views.add_food_log, name="add_food_log"),
 ]
--- foodtracker/views.py
+++ foodtracker/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.utils import timezone
+from django.db.models import Sum
+from django.db.models.functions import TruncDate
 from foodtracker.models import FoodLog
 from foodtracker.forms import FoodLogForm
 
@@ -8,7 +10,40 @@ from foodtracker.forms import FoodLogForm
 def list_food_logs(request):
     food_logs = FoodLog.objects.all().order_by("-feeddatetime")
     form = FoodLogForm()
-    return render(request, "foodtracker/food_log_list.html", {"food_logs": food_logs, "form": form})
+    
+    context = {
+        "food_logs": food_logs,
+        "form": form,
+        "is_htmx": request.headers.get('HX-Request') == 'true'
+    }
+    
+    if context["is_htmx"]:
+        return render(request, "foodtracker/partials/food_log_list_content.html", context)
+    return render(request, "foodtracker/food_log_list.html", context)
+
+
+def food_logs_chart(request):
+    if request.headers.get('HX-Request') == 'true':
+        return render(request, "foodtracker/partials/food_chart.html")
+    return render(request, "foodtracker/food_log_list.html", {"show_chart": True})
+
+
+def food_logs_chart_data(request):
+    # Group food logs by date and sum the quantities
+    daily_totals = FoodLog.objects.annotate(
+        date=TruncDate('feeddatetime')
+    ).values('date').annotate(
+        total_food=Sum('food_qty')
+    ).order_by('date')
+    
+    # Format the data for the chart
+    labels = [entry['date'].strftime('%Y-%m-%d') for entry in daily_totals]
+    data = [float(entry['total_food']) for entry in daily_totals]
+    
+    return JsonResponse({
+        'labels': labels,
+        'data': data
+    })
 
 
 def add_food_log(request):
@@ -18,8 +53,12 @@ def add_food_log(request):
             food_log = form.save(commit=False)
             food_log.feeddatetime = timezone.now()
             food_log.save()
-            # Render just the new row
-            return render(request, "foodtracker/partials/food_log_row.html", {"log": food_log})
+            # Return empty response for HTMX to handle the redirect
+            if request.headers.get('HX-Request') == 'true':
+                return HttpResponse(status=204, headers={
+                    'HX-Trigger': 'foodLogAdded'
+                })
+            return redirect('list_food_logs')
         else:
             # Return the form with errors
             return render(request, "foodtracker/partials/food_log_form.html", {"form": form})
